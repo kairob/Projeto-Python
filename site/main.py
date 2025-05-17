58e138c0bd90eec19dc780ae396e8ced4dad6da7
kairob <64109098+kairob@users.noreply.github.com>
2025-05-17 20:56:45 -0300

    zscgb

--- site/main.py
+++ site/main.py
@@ -1,9 +1,9 @@
-from flask  import Flask
+from flask  import Flask ,render_template
 
 app = Flask(__name__)
 @app.route('/') 
 def homepage ():
-    return 'Hello, World!'
+    return render_template('homepage.html')
 @app.route('/perfil')
 def perfil():
     return 'This is the/perfil page.'    ,
@@ -11,6 +11,6 @@ def perfil():
 def contato():
     return 'This is the contato page.'  
 
-    
+
 if __name__ == '__main__':
     app.run(debug=True)

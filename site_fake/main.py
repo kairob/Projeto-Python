78955688819721b27af8eae23477d72017f43cea
kairob <64109098+kairob@users.noreply.github.com>
2025-05-22 07:35:07 -0300

    22

--- site_fake/main.py
+++ site_fake/main.py
@@ -1,12 +1,6 @@
-from flask import Flask, render_template , url_for
-app = Flask(__name__)
+fr
 
-@app.route('/')
-def homepage():
-    return render_template('homepage.html')
-@app.route('/perfil/<usuario>')
-def perfil(usuario):
-    return render_template('perfil.html', usuario=usuario)
+
 
 
 
